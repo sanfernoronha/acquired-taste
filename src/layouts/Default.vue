2f5fc052c74e88777f0e3130ed35c1c5d6b99c70
sanfernoronha <sanfernoronha99@gmail.com>
2021-09-21 14:05:54 +0530

    :sparkles: added local files

---
+++ src/layouts/Default.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="font-sans text-gray-800 leading-normal flex flex-col min-h-screen"
+  >
+    <header class="border-t-14 border-green-500">
+      <nav class="container mx-auto flex justify-between items-center py-8">
+        <a href="" class="uppercase font-bold text-xl"
+          >Acquired <span class="text-green-500">taste</span></a
+        >
+        <ul class="uppercase tracking-wide font-bold flex items-center">
+          <li class="mr-8">
+            <a href="#" class="text-gray-800 hover:text-gray-600">Github</a>
+          </li>
+          <li>
+            <a href="#" class="text-gray-800 hover:text-gray-600">blog</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="flex-grow">
+      <slot />
+    </div>
+
+    <footer class="bg-green-500 text-white">
+      <div class="container mx-auto flex items-center justify-between py-8">
+        <div>Copyright 2021, All rights reserved</div>
+        <ul class="flex items-center">
+          <li class="mr-8"><a href="#">A</a></li>
+          <li class="mr-8"><a href="#">B</a></li>
+          <li class="mr-8"><a href="#">C</a></li>
+        </ul>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style src="../main.css" />
---
+++ src/pages/Index.vue
@@ -0,0 +1,30 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <!-- <g-image alt="Example image" src="~/favicon.png" width="135" /> -->
+
+    <div
+      class="hero container-inner mx-auto flex flex-col sm:flex-row justify-between py-16 mt-8"
+    >
+      <div class="text-4xl font-bold w-full sm:w-3/5 text-center sm:text-left">
+        <div class="leading-tight">Music genre classification web app</div>
+        <div class="text-green-700 leading-tight">
+          Welcome to the site
+        </div>
+      </div>
+      <div class="mt-8 sm:mt-0">
+        <g-image src="~/dev.svg" alt="hero" class="mx-auto sm:mx-0" />
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style></style>
