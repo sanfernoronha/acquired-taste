302261b0da5b61e516969ff29e56c1f8a1c9dbf2
sanfernoronha <sanfernoronha99@gmail.com>
2021-09-26 16:13:48 +0530

    :wrench: fixed config file

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,9 +5,11 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 const tailwind = require("tailwindcss");
 const purgecss = require("@fullhuman/postcss-purgecss");
+const postcssPlugins = [tailwind()];
+
 if (process.env.NODE_ENV === "production")
   postcssPlugins.push(purgecss(require("./purgecss.config.js")));
-const postcssPlugins = [tailwind()];
+
 module.exports = {
   siteName: "acquired-taste",
   plugins: [],
